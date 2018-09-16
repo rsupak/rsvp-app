c8184a14dbcb56852628eb8c09be1c6fb18ba0cc
Richard Supak <richard.supak@icloud.com>
2018-09-16 16:14:51 -0500

    Add first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './App.css';
+
+const Header = props => {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+const CounterDisplay = ({onIncrement, onDecrement, score}) => (
+  <div className="counter">
+    <button className="counter-action decrement" onClick={onDecrement}> - </button>
+    <div className="counter-score"> {score} </div>
+    <button className="counter-action increment" onClick={onIncrement}> + </button>
+  </div>
+);
+
+const Counter = class extends React.Component {
+ constructor(props) {
+    super(props)
+    this.incrementScore = this.incrementScore.bind(this)
+    this.state = {
+      score: 0
+    } //initial counter value
+ }
+ incrementScore = () => this.setState({score: score + 1})
+ render = () => 
+   <CounterDisplay score={this.state} onIncrement={this.incrementScore} onDecrement={this.decrementScore} />
+}
+
+const Player = props => {
+    return (
+      <div className="player">
+        <div className="player-name">
+          {props.name}
+        </div>
+        <div className="player-score">
+          <Counter/>
+        </div>
+      </div>
+    )
+}
+
+Player.propTypes = {
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+}
+
+const App = (props) => {
+    return (
+      <div className="scoreboard">
+        <Header title={props.title}/>
+        <div className="players">
+          {props.players.map((player) => {
+            return (
+              <Player name={player.name} 
+                      score={player.score} 
+                      key={player.id}/>
+            )
+          })}
+        </div>
+      </div>
+    )
+}
+
+App.propTypes = {
+  title: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
+};
+
+App.defaultProps = {
+  title: "Scoreboard",
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+var PLAYERS = [
+  {
+    name: "Rich Supak",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Lace Supak",
+    score: 33,
+    id: 2,
+  },
+  {
+    name: "Jules Supak",
+    score: 27,
+    id: 3,
+  },
+];
+
+ReactDOM.render(<App players={PLAYERS}/>, document.getElementById('root'));
+registerServiceWorker();
